10df916b03cdc44640379947d54cc81bb6b2ca66
Ifeanyi Nwodo <joshuasearch@gmail.com>
2021-05-04 10:57:28 +0100

    update for log

--- Microservice-Producer/Microservice-Producer/Model/MongoOptions.cs
+++ Microservice-Producer/Microservice-Producer/Model/MongoOptions.cs
@@ -9,7 +9,6 @@ namespace Microservice_Producer.Model
     {
         public string ConnectionString { get; set; }
         public string Database { get; set; }
-
         public string Collection { get; set; }
     }
 }
